033df7ff39004a2dbf9328da74c675e1f8afb300
Hhaeum <yellowgreeting@gmail.com>
2019-05-31 20:20:07 +0900

    a

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Blog
+from django.contrib import admin
+from .models import Blog,Profile 
+
+admin.site.register(Blog)
+admin.site.register(Profile)
+
+# Register your models here.
